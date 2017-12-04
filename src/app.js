9e920a792770cc942cb1dd44aae6417d9d627552
Bacto <adrien@bacto.net>
2017-12-04 22:19:24 +0100

    Catch errors to avoid exit if API is not working

--- src/app.js
+++ src/app.js
@@ -48,57 +48,62 @@ import delay from 'timeout-as-promise';
   // Infinite loop
   while (true) {
 
-    // Get trades from Cryptowat's API
-    const trades = await request(
-      'https://api.cryptowat.ch/markets/kraken/btceur/ohlc',
-      {
-        json: true,
-        qs: {
-          periods: candlesSeconds, // number of seconds
-          after: Math.round(Date.now() / 1000) - candlesSeconds * candles, // since this unixtime
-          before: Math.round(Date.now() / 1000), // until this unixtime (current time)
-        }
+    try {
+      // Get trades from Cryptowat's API
+      const trades = await request(
+        'https://api.cryptowat.ch/markets/kraken/btceur/ohlc',
+        {
+          json: true,
+          qs: {
+            periods: candlesSeconds, // number of seconds
+            after: Math.round(Date.now() / 1000) - candlesSeconds * candles, // since this unixtime
+            before: Math.round(Date.now() / 1000), // until this unixtime (current time)
+          }
+        });
+
+
+      // Format the trades received
+      // [ CloseTime, OpenPrice, HighPrice, LowPrice, ClosePrice, Volume ]
+      const datas = trades.result['60'].map(e => e[4]);
+
+      // Get MACD statistics
+      // const result = macd(datas, 10, 26, 9);
+      const result = MACD.calculate({
+        values: datas,
+        fastPeriod: 10,
+        slowPeriod: 26,
+        signalPeriod: 9,
+        SimpleMAOscillator: false,
+        SimpleMASignal: false
       });
 
+      // Get the last candle close price
+      const lastCandleClosePrice = datas[datas.length - 1];
 
-    // Format the trades received
-    // [ CloseTime, OpenPrice, HighPrice, LowPrice, ClosePrice, Volume ]
-    const datas = trades.result['60'].map(e => e[4]);
+      // Get the last histogram value
+      const lastResult = result[result.length - 1];
+      const lastHistogramValue = lastResult.histogram;
 
-    // Get MACD statistics
-    // const result = macd(datas, 10, 26, 9);
-    const result = MACD.calculate({
-      values: datas,
-      fastPeriod: 10,
-      slowPeriod: 26,
-      signalPeriod: 9,
-      SimpleMAOscillator: false,
-      SimpleMASignal: false
-    });
+      // Set action to buy if the last histogram value if positive, sale if negative
+      const action = lastHistogramValue > 0 ? 'buy' : 'sale';
 
-    // Get the last candle close price
-    const lastCandleClosePrice = datas[datas.length - 1];
+      // Send a message if action is different from the last action we defined
+      if (lastValues.action && lastValues.action !== action) {
+        const message = `${new Date()} - ${lastCandleClosePrice.toFixed(2).toString().padStart(8)} - ${action}`;
+        console.log(message);
 
-    // Get the last histogram value
-    const lastResult = result[result.length - 1];
-    const lastHistogramValue = lastResult.histogram;
-
-    // Set action to buy if the last histogram value if positive, sale if negative
-    const action = lastHistogramValue > 0 ? 'buy' : 'sale';
-
-    // Send a message if action is different from the last action we defined
-    if (lastValues.action && lastValues.action !== action) {
-      const message = `${new Date()} - ${lastCandleClosePrice.toFixed(2).toString().padStart(8)} - ${action}`;
-      console.log(message);
-
-      // Send the message to every user in "users"
-      for (const nick in users) {
-        bot.telegram.sendMessage(users[nick], message);
+        // Send the message to every user in "users"
+        for (const nick in users) {
+          bot.telegram.sendMessage(users[nick], message);
+        }
       }
-    }
 
-    // Save the last values we received
-    lastValues = { action, lastCandleClosePrice, lastHistogramValue };
+      // Save the last values we received
+      lastValues = { action, lastCandleClosePrice, lastHistogramValue };
+    }
+    catch (error) {
+      console.warn(error);
+    }
 
     // Waiting 60 seconds until we start over the loop (while)
     await delay(60 * 1000);
